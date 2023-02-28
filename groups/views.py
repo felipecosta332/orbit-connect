757b6ea2e3e1a9968e417422c96b8b26c0b4f45c
Felipe Costa <felipecosta776@outlook.com>
2023-02-28 16:47:55 -0300

    creates first view sketches for Groups

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from groups.models import Group
+from groups.models import GroupMember
 
 # Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin, CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(DetailView):
+    model = Group
+
+
+class ListGroups(ListView):
+    model = Group
