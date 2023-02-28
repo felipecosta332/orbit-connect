9789dfab8a659448f753748fbea7809b6f095a6a
Felipe Costa <felipecosta776@outlook.com>
2023-02-28 11:12:17 -0300

    creates SignUp view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from accounts import forms
 
 # Create your views here.
+
+
+class SignUp(CreateView):
+    pass
