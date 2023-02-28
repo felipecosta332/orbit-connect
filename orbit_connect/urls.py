8ca941eea6be91a023e44ab5f8401d3a5e85bc86
Felipe Costa <felipecosta776@outlook.com>
2023-02-28 11:03:07 -0300

    creates homepage

--- orbit_connect/urls.py
+++ orbit_connect/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from django.urls import include
+from orbit_connect import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', views.HomePage.as_view(), name='home'),
 ]
